d37f345f47fcc686f26f85727a7419bc7be50298
gong-ik <aruesin2@gmail.com>
2020-05-14 17:59:19 +0900

    브랜치

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -13,15 +13,15 @@ int main() {
     cin >> str;
     for(int i=0; i<str.length(); i++) {
         for(int j=str.length()-1; j-i>0; j--) {
-            if( (j-i+1) < max1 )
+            if( (j-i+1) < max1 ) //백트래킹? 이라해야되나
                 break;
-            if(str[i]==str[j]) {
+            if(str[i]==str[j]) { //1번째와 n-1번쨰가 같으면 , 2번쨰와 n-2번쨰가 같으면 ...
                 if((j-i+1)%2 == 0) { //짝수이면
                     int left = i;
                     int right = j;
                     
-                    while(true) {
-                        if(str[left] == str[right]) {
+                    while(true) { //1번째와 n-1번째,2번째와 n-2번째 ... 가 다르면 break
+                        if(str[left] == str[right]) { 
                             sameNum +=2;
                         }
                         else{
@@ -31,9 +31,9 @@ int main() {
                         left++;
                         right--;
 
-                        if(left>right) {
-                            if(max1 <=sameNum) {
-                                max1 = sameNum;
+                        if(left>right) { //max값이 더크고 마지막 까지 성공적으로 탐색할경우
+                            if(max1 <=sameNum) { 
+                                max1 = sameNum; //
                             }
                             sameNum = 0;
                             break;
